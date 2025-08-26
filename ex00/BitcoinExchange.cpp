21e5cb4fa4e9c6a3f1c836e5cba10f96b0e571fe
Adambrhh <adam.barahhou@proton.me>
2025-08-26 16:21:08 +0200

    started

---
+++ ex00/BitcoinExchange.cpp
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 15:47:03 by abarahho          #+#    #+#             */
+/*   Updated: 2025/08/26 16:15:42 by abarahho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::~BitcoinExchange() {}
---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarahho <abarahho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 15:46:46 by abarahho          #+#    #+#             */
+/*   Updated: 2025/08/26 16:15:21 by abarahho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once "BitcoinExchange.hpp"
+
+class BitcoinExchange
+{
+private:
+
+public:
+    BitcoinExchange();
+    ~BitcoinExchange();
+};
